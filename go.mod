c7726f6a61bc4fc6d04fa8a17d7345e5159f4712
Daniel <dflyboy420@gmail.com>
2024-12-07 16:48:35 +0100

    part 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dflyboy.me/day1
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("in.txt")
+	if err != nil {
+		log.Fatal("error opening file", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var txtLines []string
+
+	for scanner.Scan() {
+		txtLines = append(txtLines, scanner.Text())
+	}
+
+	file.Close()
+
+	var leftList, rightList []int
+
+	for _, line := range txtLines {
+		numbers := strings.Fields(line)
+		if len(numbers) != 2 {
+			log.Fatal("wrong number of fields in line")
+		}
+
+    left, err := strconv.Atoi(numbers[0])
+		if err != nil {
+			log.Fatal("error converting number", err)
+		}
+
+		right, err := strconv.Atoi(numbers[1])
+		if err != nil {
+			log.Fatal("error converting number", err)
+		}
+
+    leftList = append(leftList, left)
+		rightList = append(rightList, right)
+	}
+
+  if(len(leftList) != len(rightList)) {
+    log.Fatal("lengths don't match")
+  }
+
+  slices.Sort(leftList)
+  slices.Sort(rightList)
+
+  dSum := 0.0
+
+  for i := range len(leftList) {
+    d := math.Abs(float64(leftList[i]) - float64(rightList[i]))
+    dSum += d
+  }
+
+  fmt.Println(dSum)
+
+}
